3153c36dba86bd7776c58c421c76be0523f881a7
José García V <jose3500@gmail.com>
2012-02-06 12:51:24 -0600

    Pytterhub Terminado

---
+++ pytterhub/ToDo.py
@@ -0,0 +1,5 @@
+calendar = {}
+#Diccionario dentro de otro Diccionario
+calendar['04022012'] = {'12:00' : 'Parar de programar', '12:15' : 'Regresar a programar', '14:56' : 'Corriendo'}
+calendar['04022012'] = {'13:23' : 'Salir a caminar'}
+calendar['04022012'] = {'15:00' : 'Ir de Compras' , '17:17' : 'Ir con blankiss'}
---
+++ pytterhub/settings.py
@@ -0,0 +1,9 @@
+OAUTH_SETTINGS = {}
+OAUTH_SETTINGS['consumer_key'] = 'W6onSMh5TaAdSuB0JrDHA' 
+OAUTH_SETTINGS['consumer_secret'] = 'utGDF0pQ2yDeTlTjer1pkP9bs6n8qjTGFcT6N1doaW8' 
+OAUTH_SETTINGS['oauth_token'] = '85917075-L9OwWzm2k37iukS4lYfd4FEQZwUQZOVUvPef8O8kW' 
+OAUTH_SETTINGS['oauth_token_secret'] = 'uypscXmrYS8vNVDDdUze4NVQRnsKUl24epuUy0rKw' 
+
+TWITTER_SETTINGS = {}
+TWITTER_SETTINGS['bot'] = 'pytterhub' #Se debe que tener un usuario en twitter que sigas y que te siga llamado pytterhub
+TWITTER_SETTINGS['user'] = 'Jose_3500'
